690ae70eff84a161a13725ecc006dde780105e68
Faiz Khalifah <faiz_doc@outlook.com>
2025-06-05 21:39:54 +0700

    lesson 8 and 9

---
+++ lesson8.py
@@ -0,0 +1,7 @@
+angka = 0
+while(angka<=100):
+    if(angka==70):
+        angka+=1
+        continue
+    print(angka)
+    angka+=1
---
+++ lesson9.py
@@ -0,0 +1,9 @@
+def pertambahan(angka1, angka2):
+    if(angka1<0):
+        print("angka tidak boleh dibawah nol")
+    return angka1 + angka2
+
+print(pertambahan(10,8))
+print(pertambahan(11,89))
+print(pertambahan(8,3))
+print(pertambahan(1000,212))
